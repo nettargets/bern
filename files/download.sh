c10a43f473da0fdf730204487bc111964311e423
minkyushim <minkyu.shim@net-targets.com>
2021-06-22 14:22:08 +0000

    https://nettargets.atlassian.net/browse/DV20-280 Archiving BERN-related files

---
+++ files/download.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# download crfpp
+wget -nc -O ./crfpp-0.58.tar.gz https://drive.google.com/uc?id=0B4y35FiV1wh7QVR6VXJ5dWExSTQ
+
+# download GnormPlusJava
+wget -nc https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/tmTools/download/GNormPlus/GNormPlusJava.zip
+
+# download GNormPlusServer.jar
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1g-JlhqeDIlZX5YFk8Y27_M8BXUXcQRSX' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1g-JlhqeDIlZX5YFk8Y27_M8BXUXcQRSX" -O GNormPlusServer.jar &&     rm -rf /tmp/cookies.txt
+
+# download tmVarJava
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/lu/Suppl/tmVar2/tmVarJava.zip
+
+# download tmVar2Server.jar
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kQYzLHLFLsU9qKpRRGjXkIYmaYK6bPJm' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1kQYzLHLFLsU9qKpRRGjXkIYmaYK6bPJm" -O tmVar2Server.jar && rm -rf /tmp/cookies.txt
+
+# download sqlite-jdbc-3.20.0.jar
+wget -nc https://repo1.maven.org/maven2/org/xerial/sqlite-jdbc/3.20.0/sqlite-jdbc-3.20.0.jar
+
+# download stanford-corenlp-3.5.2.jar
+wget -nc https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.5.2/stanford-corenlp-3.5.2.jar
+
+# download resources.zip
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uU1U6UORqr3l_YYQ5TXeazpLrpeg_OcP' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1uU1U6UORqr3l_YYQ5TXeazpLrpeg_OcP" -O resources.zip && rm -rf /tmp/cookies.txt
+
+# download biobert_ner_models.zip
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1sSVEqvMBVLj1RJmlQDhRKyt_oe-wc5LK' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1sSVEqvMBVLj1RJmlQDhRKyt_oe-wc5LK" -O biobert_ner_models.zip && rm -rf /tmp/cookies.txt
+
--- supplementary/download.sh
+++ supplementary/download.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
-gdown(){
-        id=$1
-        filename=$2
-        echo "Downloading $2 with id,$1."
-        wget --load-cookies "/tmp/cookies_$id.txt" "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies_$id.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$id' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$id" -O $filename && rm -rf "/tmp/cookies_$id.txt"
-}
+####################################
+# Download Supplemantary Data      #
+####################################
 
 # Download BERN annotated data
-# https://drive.google.com/open?id=14YrlOGd1NdDn0XD-Yat4bbq3lRv1EyqR
-gdown 14YrlOGd1NdDn0XD-Yat4bbq3lRv1EyqR 2019_merged_json_fixed.zip
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=14YrlOGd1NdDn0XD-Yat4bbq3lRv1EyqR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=14YrlOGd1NdDn0XD-Yat4bbq3lRv1EyqR" -O 2019_merged_json_fixed.zip && rm -rf /tmp/cookies.txt
 
 # Dictionaries
+
 # chem_meta_190806.tsv
 # https://drive.google.com/file/d/1zq-za_1OMCrrJaVwIj-dwIHogqVQ9n0G/view?usp=sharing
-gdown 1zq-za_1OMCrrJaVwIj-dwIHogqVQ9n0G chem_meta_190806.tsv
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zq-za_1OMCrrJaVwIj-dwIHogqVQ9n0G' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zq-za_1OMCrrJaVwIj-dwIHogqVQ9n0G" -O chem_meta_190806.tsv && rm -rf /tmp/cookies.txt
 
 # chem_meta.tsv
 # https://drive.google.com/file/d/1RhyFlPEyGPqI840d5TkXh2DAR1WR7YqL/view?usp=sharing
-gdown 1RhyFlPEyGPqI840d5TkXh2DAR1WR7YqL chem_meta.tsv
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1RhyFlPEyGPqI840d5TkXh2DAR1WR7YqL' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1RhyFlPEyGPqI840d5TkXh2DAR1WR7YqL" -O chem_meta.tsv && rm -rf /tmp/cookies.txt
 
 # disease_meta_190805.tsv
 # https://drive.google.com/file/d/1guHxBbUksuDx58zKh8o0d0dgs7klotFT/view?usp=sharing
-gdown 1guHxBbUksuDx58zKh8o0d0dgs7klotFT disease_meta_190805.tsv
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1guHxBbUksuDx58zKh8o0d0dgs7klotFT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1guHxBbUksuDx58zKh8o0d0dgs7klotFT" -O disease_meta_190805.tsv && rm -rf /tmp/cookies.txt
 
 # disease_meta_desc_190713.tsv
 # https://drive.google.com/file/d/1i5sWSRt73DYEFNPU_Hb3d83X18QMYc3L/view?usp=sharing
-gdown 1i5sWSRt73DYEFNPU_Hb3d83X18QMYc3L disease_meta_desc_190713.tsv 
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1i5sWSRt73DYEFNPU_Hb3d83X18QMYc3L' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1i5sWSRt73DYEFNPU_Hb3d83X18QMYc3L" -O disease_meta_desc_190713.tsv && rm -rf /tmp/cookies.txt
 
 # gene_extids_190510.tsv
 # https://drive.google.com/file/d/1KgJPBYB8D4_hN7wbiu0XOOM-lQdV8EgP/view?usp=sharing
-gdown 1KgJPBYB8D4_hN7wbiu0XOOM-lQdV8EgP gene_extids_190510.tsv
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KgJPBYB8D4_hN7wbiu0XOOM-lQdV8EgP' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1KgJPBYB8D4_hN7wbiu0XOOM-lQdV8EgP" -O gene_extids_190510.tsv && rm -rf /tmp/cookies.txt
 
 # gene_meta_190805.tsv
 # https://drive.google.com/file/d/1tVYRh3bQ9TzpAlVPLVii4drafzC2EUCB/view?usp=sharing
-gdown 1tVYRh3bQ9TzpAlVPLVii4drafzC2EUCB gene_meta_190805.tsv
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1tVYRh3bQ9TzpAlVPLVii4drafzC2EUCB' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1tVYRh3bQ9TzpAlVPLVii4drafzC2EUCB" -O gene_meta_190805.tsv && rm -rf /tmp/cookies.txt
 
 # README_species.txt
 # https://drive.google.com/file/d/1RGaTaE6dJhofuxhvtWZZ1VIeHwYl-IaG/view?usp=sharing
-gdown 1RGaTaE6dJhofuxhvtWZZ1VIeHwYl-IaG README_species.txt
+wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1RGaTaE6dJhofuxhvtWZZ1VIeHwYl-IaG' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1RGaTaE6dJhofuxhvtWZZ1VIeHwYl-IaG" -O README_species.txt && rm -rf /tmp/cookies.txt
+
